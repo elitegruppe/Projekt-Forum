ef4f2e674915998b79b680d8cadac3402dd926e8
elitegruppe <a.villars@bluewin.ch>
2016-09-28 15:03:42 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<!-- MetaTags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="HTML, CSS, PHP, JAVASCRIPT">
+    <meta name="author" content="root" >
+    <link rel=icon href=favicon.jpg sizes="16x16" type="image/png">
+
+    <title>BVZ-Forum</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="login.php">Home</a>
+          <a class="blog-nav-item" href="forum.php">Forum</a>
+          <a class="blog-nav-item" href="about.php">About</a>
+        </nav>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="blog-header">
+        <h1 class="blog-title">&Uumlber uns</h1>
+        <p class="lead blog-description">Diese Seite wurde unter Stress verfasst</p>
+      </div>
+      
+      
+
+      <div class="row">
+
+        <div class="col-sm-8 blog-main">
+
+
+        </div><!-- /.blog-main -->
+
+
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>Written by BVZ</p>
+      <p>
+        <a href="login.php">Back to Home</a>
+      </p>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ forum.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="HTML, CSS, PHP, JAVASCRIPT">
+    <meta name="author" content="root" >
+    <link rel=icon href=favicon.jpg sizes="16x16" type="image/png">
+
+    <title>BVZ-Forum</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="login.php">Home</a>
+          <a class="blog-nav-item" href="forum.php">Forum</a>
+          <a class="blog-nav-item" href="about.php">About</a>
+        </nav>
+      </div>
+    </div>
+	 <div class="linke-box">
+			  <ol class="linke-box">
+              <li><a href="">Thema 1</a></li>
+              <li><a href="">Thema 2</a></li>
+              <li><a href="">Thema 3</a></li>
+              <li><a href="">Thema 4</a></li>
+              <li><a href="">Thema 5</a></li>
+              <li><a href="">Thema 6</a></li>
+              <li><a href="">Thema 7</a></li>
+              <li><a href="">Thema 8</a></li>
+              <li><a href="">Thema 9</a></li>
+           </ol>  
+      </div>
+    <div class="container">
+
+      <div class="blog-header">
+        <h1 class="blog-title">BVZ-Forum</h1>
+      </div>
+
+      <div class="row">
+
+        <div class="col-sm-8 blog-main">
+
+         
+        </div><!-- /.blog-main -->
+
+        <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+        
+        </div><!-- /.blog-sidebar -->
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>Written by BVZ</p>
+      <p>
+        <a href="login.php">Back to Home</a>
+      </p>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
